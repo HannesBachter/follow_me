4f0856bf88d5bcab2411498da066c2692591fabf
ipa-fmw <florian.weisshardt@ipa.fraunhofer.de>
2015-12-09 13:01:30 +0100

    update tf broadcaster and add launch files

--- src/eband_moving_simple.cpp
+++ src/eband_moving_simple.cpp
@@ -7,6 +7,8 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 
 int main(int argc, char** argv){
   ROS_INFO("ROS Init");
+  //std::string target_frame = "pcl_0";
+  std::string target_frame = "reference1";
   ros::init(argc, argv, "eband_moving_simple");
     //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -20,7 +22,7 @@ int main(int argc, char** argv){
   //Parameter setzen, damit EBand Planer's catch moving goal ausgeführt wird
   ros::NodeHandle n;
   n.setParam("/move_base/EBandPlannerROS/catch_moving_goal", true);
-  n.setParam("/move_base/moving_target_frame", "ragdoll_pos");
+  n.setParam("/move_base/moving_target_frame", target_frame);
   float DISTANCE = 1.0; //distance to target
 
   float pos_arr[4]={0.0,0.0,0.0,0.0};
@@ -28,18 +30,19 @@ int main(int argc, char** argv){
 
   //while(n.ok()){
   //send a goal to the robot
-  goal.target_pose.header.frame_id = "ragdoll_pos";
+  goal.target_pose.header.frame_id = target_frame;
   goal.target_pose.header.stamp = ros::Time::now();
 
 
-	  goal.target_pose.pose.position.x = pos_arr[0];
-	  goal.target_pose.pose.position.y = pos_arr[1];
+	  goal.target_pose.pose.position.x = 0;
+	  goal.target_pose.pose.position.y = 0;
 	  
 	  goal.target_pose.pose.orientation.x = 0;//4.0 * atan2(pos_arr[1],pos_arr[0]);
 	  goal.target_pose.pose.orientation.y = 0;//4.0 * atan2(pos_arr[1],pos_arr[0]);
 	  goal.target_pose.pose.orientation.z = 0;//roundf(sin(atan2(pos_arr[1],pos_arr[0])/2)*100)/100.0;
 	  goal.target_pose.pose.orientation.w = 1;// roundf(cos(atan2(pos_arr[1],pos_arr[0])/2)*100)/100.0;
 
+      ros::Duration(3).sleep();
 	  ROS_INFO("Sending goal");
 	  ac.sendGoal(goal);
 
